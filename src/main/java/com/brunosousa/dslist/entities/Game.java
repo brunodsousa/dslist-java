90af77ecefc58a0b92f73387029dee5173add4ed
Bruno Sousa <brunodsousa@outlook.com>
2025-05-22 21:45:31 -0300

    fix setters, Game seed

--- src/main/java/com/brunosousa/dslist/entities/Game.java
+++ src/main/java/com/brunosousa/dslist/entities/Game.java
@@ -14,19 +14,23 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {}
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -36,72 +40,72 @@ public class Game {
         return id;
     }
 
-    public Game setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
-        return this;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public Game setTitle(String title) {
+    public void setTitle(String title) {
         this.title = title;
-        return this;
     }
 
     public Integer getYear() {
         return year;
     }
 
-    public Game setYear(Integer year) {
+    public void setYear(Integer year) {
         this.year = year;
-        return this;
     }
 
     public String getGenre() {
         return genre;
     }
 
-    public Game setGenre(String genre) {
+    public void setGenre(String genre) {
         this.genre = genre;
-        return this;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public Game setPlatform(String platform) {
-        this.platform = platform;
-        return this;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
         return imgUrl;
     }
 
-    public Game setImgUrl(String imgUrl) {
+    public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
-        return this;
     }
 
     public String getShortDescription() {
         return shortDescription;
     }
 
-    public Game setShortDescription(String shortDescription) {
+    public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
-        return this;
     }
 
     public String getLongDescription() {
         return longDescription;
     }
 
-    public Game setLongDescription(String longDescription) {
+    public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
-        return this;
     }
 
     @Override
